9476b41bf923a2a131a94b35cbeb5f497d9aa719
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-10-29 00:15:54 +0500

    Initial commit

---
+++ app/src/main/java/org/abubaker/clippingexample/ClippedView.kt
@@ -0,0 +1,15 @@
+package org.abubaker.clippingexample
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class ClippedView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+
+
+}
